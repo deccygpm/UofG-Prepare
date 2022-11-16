abcdc7e72fbac7c9d52106af240ac330d909e7c3
Declan McBride <2399448m@student.gla.ac.uk>
2022-11-16 11:44:10 +0000

    Tidied up profile UI

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:l2_transition/services/auth.dart';
 import 'package:l2_transition/shared/shared.dart';
 
@@ -8,27 +9,35 @@ class UserProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(),
-      drawer: const CustomDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Center(
-          child: Column(children: [
-            Image.asset(
-              'assets/image/uofg-logo-large.jpg',
-              scale: 3,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, '/', (route) => false);
-                },
-                child: const Text("Logout")),
-          ]),
-        ),
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          return Scaffold(
+              appBar: const CustomAppBar(),
+              drawer: const CustomDrawer(),
+              body: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/image/uofg-logo-large.jpg',
+                        scale: 3,
+                      ),
+                      SignInButtonBuilder(
+                        backgroundColor: Colors.white,
+                        onPressed: () async {
+                          await FirebaseAuth.instance.signOut();
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, '/', (route) => false);
+                        },
+                        text: "Logout",
+                        icon: Icons.verified_user_outlined,
+                        iconColor: Colors.black,
+                        textColor: Colors.black,
+                      )
+                    ],
+                  ))));
+        });
   }
 }
