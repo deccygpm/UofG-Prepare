ba8e8f4dff76b71ec4c7777ec7149b85c84d2600
Declan McBride <2399448m@student.gla.ac.uk>
2022-12-03 16:02:50 +0000

    [#13] Rebuilt registration page

--- lib/login/registration.dart
+++ lib/login/registration.dart
@@ -32,113 +32,144 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: const CustomAppBar(),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Form(
-            key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Headline(
-                  data: 'Register',
-                  color: themeGrey,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 3.0,
-                      )),
-                  child: Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: TextFormField(
-                          controller: emailController,
-                          textInputAction: TextInputAction.next,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: const InputDecoration(labelText: "Email"),
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (email) =>
-                              ValidationService().validateEmail(email!))),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 3.0,
-                        )),
-                    child: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: TextFormField(
-                            controller: passwordController,
-                            obscureText: true,
-                            textInputAction: TextInputAction.done,
-                            decoration:
-                                const InputDecoration(labelText: "Password"),
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            validator: (password) => ValidationService()
-                                .validatePassword(password!))),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 5),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      final isValid = formKey.currentState!.validate();
-                      if (!isValid) return;
-
-                      showDialog(
-                          context: context,
-                          barrierDismissible: true,
-                          builder: (context) => const Center(
-                                child: CircularProgressIndicator(),
-                              ));
-
-                      AuthService().emailSignUp(emailController.text.trim(),
-                          passwordController.text.trim());
-
-                      navigatorKey.currentState!.pop();
-                    },
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      side: const BorderSide(color: Colors.black, width: 3),
-                    ),
-                    child: const Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
-                const Divider(
-                  color: Colors.black,
-                ),
-                Center(
-                  heightFactor: 2,
-                  child: RichText(
-                      text: TextSpan(
-                          text: "Already have an account? ",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.black),
-                          children: [
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = widget.onClickedSignIn,
-                          text: "Click here to Sign In",
-                          style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            decoration: TextDecoration.underline,
-                            color: Colors.black,
+        body: Column(
+          children: [
+            Spacer(),
+            Container(
+              color: Colors.transparent,
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                      ),
+                    ],
+                    color: themeBlue,
+                    border: Border.all(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(8)),
+                margin: EdgeInsets.all(10),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      right: 10, left: 10, top: 5, bottom: 5),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5),
+                          child: Center(
+                            child: Headline(
+                              data: 'Register',
+                              color: themeGrey,
+                            ),
                           ),
                         ),
-                      ])),
-                )
-              ],
+                        Container(
+                          decoration: BoxDecoration(
+                              color: themeGrey,
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 3.0,
+                              )),
+                          child: Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: TextFormField(
+                                  controller: emailController,
+                                  textInputAction: TextInputAction.next,
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration:
+                                      const InputDecoration(labelText: "Email"),
+                                  autovalidateMode:
+                                      AutovalidateMode.onUserInteraction,
+                                  validator: (email) => ValidationService()
+                                      .validateEmail(email!))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: themeGrey,
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 3.0,
+                                )),
+                            child: Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: TextFormField(
+                                    controller: passwordController,
+                                    obscureText: true,
+                                    textInputAction: TextInputAction.done,
+                                    decoration: const InputDecoration(
+                                        labelText: "Password"),
+                                    autovalidateMode:
+                                        AutovalidateMode.onUserInteraction,
+                                    validator: (password) => ValidationService()
+                                        .validatePassword(password!))),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              final isValid = formKey.currentState!.validate();
+                              if (!isValid) return;
+
+                              showDialog(
+                                  context: context,
+                                  barrierDismissible: true,
+                                  builder: (context) => const Center(
+                                        child: CircularProgressIndicator(),
+                                      ));
+
+                              AuthService().emailSignUp(
+                                  emailController.text.trim(),
+                                  passwordController.text.trim());
+
+                              navigatorKey.currentState!.pop();
+                            },
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: themeGrey,
+                              side: const BorderSide(
+                                  color: Colors.black, width: 3),
+                            ),
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
+            Center(
+              heightFactor: 2,
+              child: RichText(
+                  text: TextSpan(
+                      text: "Already have an account? ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: themeBlue),
+                      children: [
+                    TextSpan(
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = widget.onClickedSignIn,
+                      text: "Click here to Sign In",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        decoration: TextDecoration.underline,
+                        color: themeBlue,
+                      ),
+                    ),
+                  ])),
+            ),
+            Spacer(flex: 3)
+          ],
         ));
   }
 }
