79a679439d38e2e2ab2387fb05501c222547bb09
Declan McBride <2399448m@student.gla.ac.uk>
2022-12-03 15:00:33 +0000

    [#13] Updated system-wide font

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,17 +3,16 @@ import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
     canvasColor: const Color.fromARGB(255, 205, 205, 205),
-    fontFamily: GoogleFonts.roboto().fontFamily,
+    fontFamily: GoogleFonts.bitter().fontFamily,
     inputDecorationTheme: const InputDecorationTheme(
         enabledBorder: UnderlineInputBorder(
-        borderSide: BorderSide(color: Colors.transparent)
-      ),
+            borderSide: BorderSide(color: Colors.transparent)),
         focusedBorder: UnderlineInputBorder(
           borderSide: BorderSide(color: Colors.transparent),
         ),
         labelStyle: TextStyle(
-      color: Colors.black,
-    )),
+          color: Colors.black,
+        )),
     textSelectionTheme: const TextSelectionThemeData(
       cursorColor: Colors.black,
     ));
