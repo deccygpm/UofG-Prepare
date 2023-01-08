7e99db189569eb629b29befe056035461db8370e
Declan McBride <2399448m@student.gla.ac.uk>
2023-01-08 20:32:59 +0000

    [#27] Added tabbar

--- lib/body/study/study_menu.dart
+++ lib/body/study/study_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:l2_transition/services/local_data.dart';
+import 'package:l2_transition/theme.dart';
 
 class StudyMenuScreen extends StatefulWidget {
   const StudyMenuScreen({super.key});
@@ -11,13 +12,29 @@ class StudyMenuScreen extends StatefulWidget {
 class _StudyMenuScreenState extends State<StudyMenuScreen> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text("clear prefs"),
-      onPressed: () {
-        LocalData().clearAll();
-        Navigator.pushNamedAndRemoveUntil(
-            context, '/selection', (route) => false);
-      },
+    return DefaultTabController(
+      length: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Material(
+              color: themeBlue,
+              child: TabBar(
+                indicatorColor: Colors.transparent,
+                tabs: [
+                  Tab(
+                    text: 'Programming',
+                  ),
+                  Tab(
+                    text: 'Python',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
