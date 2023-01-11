c74d4ab5a6e8498b996960a344f0977e148ad5ee
Declan McBride <2399448m@student.gla.ac.uk>
2023-01-11 14:09:08 +0000

    [#27] Select school UI improved

--- lib/body/select_school.dart
+++ lib/body/select_school.dart
@@ -3,6 +3,7 @@ import 'package:l2_transition/services/firestore.dart';
 import 'package:l2_transition/services/local_data.dart';
 import 'package:l2_transition/services/models.dart';
 import 'package:l2_transition/shared/shared.dart';
+import 'package:l2_transition/theme.dart';
 
 class SelectSchoolScreen extends StatelessWidget {
   const SelectSchoolScreen({
@@ -25,19 +26,46 @@ class SelectSchoolScreen extends StatelessWidget {
 
             return Scaffold(
               appBar: const CustomAppBar(),
-              body: ListView.separated(
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(schools[index].fullName),
-                      onTap: () {
-                        LocalData().setSchool(schools[index].id);
-                        Navigator.pushNamedAndRemoveUntil(
-                            context, '/selection', (route) => false);
-                      },
-                    );
-                  },
-                  separatorBuilder: (context, index) => const Divider(),
-                  itemCount: schools!.length),
+              body: Container(
+                color: themeBlue,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Headline(data: 'Select School', color: themeGrey),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.4),
+                              spreadRadius: 2,
+                              blurRadius: 3,
+                            ),
+                          ],
+                          color: themeGrey,
+                          border: Border.all(color: Colors.transparent),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        margin: EdgeInsets.all(20),
+                        child: ListView.separated(
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(schools[index].fullName),
+                                onTap: () {
+                                  LocalData().setSchool(schools[index].id);
+                                  Navigator.pushNamedAndRemoveUntil(
+                                      context, '/selection', (route) => false);
+                                },
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                const Divider(),
+                            itemCount: schools!.length),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             );
           } else {
             return const Text('Database Problem');
