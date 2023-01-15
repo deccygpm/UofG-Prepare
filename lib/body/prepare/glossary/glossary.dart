b5b9bf4a613f51199843fe6bf7978d7022ee0d64
Declan McBride <2399448m@student.gla.ac.uk>
2023-01-15 17:36:11 +0000

    [#30] Glossary complete

--- lib/body/prepare/glossary/glossary.dart
+++ lib/body/prepare/glossary/glossary.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:l2_transition/body/prepare/glossary/glossary_tile.dart';
 import 'package:l2_transition/services/firestore.dart';
-import 'package:l2_transition/shared/app_bar.dart';
-import 'package:l2_transition/shared/loading.dart';
+import 'package:l2_transition/shared/shared.dart';
+import 'package:l2_transition/theme.dart';
 
-class GlossaryScreen extends StatelessWidget {
+class GlossaryScreen extends StatefulWidget {
   const GlossaryScreen({super.key});
 
   @override
+  State<GlossaryScreen> createState() => _GlossaryScreenState();
+}
+
+class _GlossaryScreenState extends State<GlossaryScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar(),
+        appBar: const CustomAppBar(),
         body: FutureBuilder(
           future: FirestoreService().getGlossary(),
           builder: (context, snapshot) {
@@ -17,11 +24,36 @@ class GlossaryScreen extends StatelessWidget {
               return const LoadingScreen();
             } else {
               var terms = snapshot.data!.terms;
-              return ListView.builder(
-                itemCount: terms.length,
-                itemBuilder: (context, index) {
-                  return Text(terms[index].term);
-                },
+              return Column(
+                children: [
+                  Headline(data: 'Glossary', color: themeBlue),
+                  Expanded(
+                    child: GroupedListView(
+                      groupSeparatorBuilder: (value) {
+                        return Container(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              value,
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: themeBlue,
+                              ),
+                            ));
+                      },
+                      groupBy: (element) => element.term[0],
+                      elements: terms,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                              left: 15, right: 15, top: 10),
+                          child: GlossaryTile(
+                            term: index,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               );
             }
           },
---
+++ lib/body/prepare/glossary/glossary_tile.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:l2_transition/services/models.dart';
+import 'package:l2_transition/shared/shared.dart';
+import 'package:l2_transition/theme.dart';
+
+class GlossaryTile extends StatefulWidget {
+  final Term term;
+  const GlossaryTile({
+    super.key,
+    required this.term,
+  });
+
+  @override
+  State<GlossaryTile> createState() => _GlossaryTileState();
+}
+
+class _GlossaryTileState extends State<GlossaryTile> {
+  bool isOpen = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: accentBlue,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
+        child: AnimatedContainer(
+          curve: Curves.fastLinearToSlowEaseIn,
+          duration: const Duration(milliseconds: 800),
+          height: isOpen ? 300 : 50,
+          child: isOpen
+              ? SingleChildScrollView(
+                  child: _TrueColumn(
+                    widget: widget,
+                  ),
+                )
+              : _FalseRow(widget: widget, isOpen: isOpen),
+        ),
+      ),
+    );
+  }
+}
+
+class _TrueColumn extends StatelessWidget {
+  const _TrueColumn({
+    Key? key,
+    required this.widget,
+  }) : super(key: key);
+
+  final GlossaryTile widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              '💡',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              widget.term.term,
+              style: TextStyle(
+                  fontSize: 18, color: themeGrey, fontWeight: FontWeight.bold),
+            ),
+            Icon(
+              Icons.arrow_downward,
+              size: 30,
+              color: themeGrey,
+            )
+          ],
+        ),
+        FancyText(data: widget.term.definition, color: themeGrey)
+      ],
+    );
+  }
+}
+
+class _FalseRow extends StatelessWidget {
+  const _FalseRow({
+    Key? key,
+    required this.widget,
+    required this.isOpen,
+  }) : super(key: key);
+
+  final GlossaryTile widget;
+  final bool isOpen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          '💡',
+          style: TextStyle(fontSize: 24),
+        ),
+        Text(
+          widget.term.term,
+          style: TextStyle(fontSize: 18, color: themeGrey),
+        ),
+        isOpen
+            ? Icon(
+                Icons.arrow_downward,
+                size: 30,
+                color: themeGrey,
+              )
+            : Icon(
+                Icons.arrow_back,
+                color: themeGrey,
+                size: 30,
+              ),
+      ],
+    );
+  }
+}
