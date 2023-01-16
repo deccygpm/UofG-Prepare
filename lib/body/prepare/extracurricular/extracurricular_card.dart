79ae2c61a142f34f2b81d7169af212dd4036e7b0
Declan McBride <2399448m@student.gla.ac.uk>
2023-01-16 13:35:34 +0000

    [#30] Extracurricular card complete

--- lib/body/prepare/extracurricular/extracurricular_card.dart
+++ lib/body/prepare/extracurricular/extracurricular_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:l2_transition/services/models.dart';
 import 'package:l2_transition/shared/shared.dart';
 import 'package:l2_transition/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ExtracurricularCard extends StatelessWidget {
   final Resource resource;
@@ -15,17 +16,41 @@ class ExtracurricularCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(20), color: accentBlue),
       child: Expanded(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              resource.name,
-              style: TextStyle(
-                  color: themeGrey, fontSize: 24, fontWeight: FontWeight.bold),
+            Center(
+              child: Text(
+                resource.name,
+                style: TextStyle(
+                    color: themeGrey,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
             FancyText(
               data: resource.description,
               color: themeGrey,
             ),
             Image.asset('assets/image/misc/gla-facade.jpg'),
+            OutlinedButton(
+              onPressed: () async {
+                var uri = Uri.parse(resource.link);
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri);
+                } else {
+                  throw 'Could not launch $uri';
+                }
+              },
+              style: OutlinedButton.styleFrom(
+                backgroundColor: themeGrey,
+                side: const BorderSide(color: Colors.black, width: 3),
+              ),
+              child: const Text(
+                "Go",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         ),
       ),
